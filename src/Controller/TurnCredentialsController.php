3dd752327dd1f329871fd3c2abc5ae9886d70c08
Stanislav Kremen <stkremen@gmail.com>
2025-04-23 22:03:41 +0200

    TURN server Metered API

--- src/Controller/TurnCredentialsController.php
+++ src/Controller/TurnCredentialsController.php
@@ -10,28 +10,19 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class TurnCredentialsController extends AbstractController
 {
-    private $httpClient;
-    private $meteredApiKey;
-
     public function __construct(
-        HttpClientInterface $httpClient,
-        string $meteredApiKey
-    ) {
-        $this->httpClient = $httpClient;
-        $this->meteredApiKey = $meteredApiKey;
-    }
+        private string $meteredUsername,
+        private string $meteredCredential
+    ) {}
 
     #[Route('/api/turn-credentials', name: 'api_turn_credentials', methods: ['GET'])]
-    public function getTurnCredentials(
-        string $meteredUsername,
-        string $meteredCredential
-    ): JsonResponse
+    public function getTurnCredentials(): JsonResponse
     {
         return $this->json([
             [
                 'urls' => 'turns:eu.relay.metered.ca:443?transport=tcp',
-                'username' => $meteredUsername,
-                'credential' => $meteredCredential
+                'username' => $this->meteredUsername,
+                'credential' => $this->meteredCredential
             ],
             [
                 'urls' => 'stun:stun.l.google.com:19302'
